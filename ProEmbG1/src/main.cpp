a2240f3c1cf3626a87217ea25af279eab72d891d
Cesarq19 <cepaquin@espol.edu.ec>
2022-12-01 20:02:56 -0500

    Configuracion inicial de la LCD i2C

--- ProEmbG1/src/main.cpp
+++ ProEmbG1/src/main.cpp
@@ -1,8 +1,18 @@
 #include <Arduino.h>
+#include <Wire.h>              //for ESP8266 use bug free i2c driver https://github.com/enjoyneering/ESP8266-I2C-Driver
+#include <LiquidCrystal_I2C.h>
+
+#define COLUMS 16
+#define ROWS   2
+
+#define PAGE   ((COLUMS) * (ROWS))
+
+LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
 
 
 void setup() {
-  // put your setup code here, to run once:
+  lcd.begin(COLUMS,ROWS);
+  
 }
 
 void loop() {
