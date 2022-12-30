bcab316106b72592f4b0229094ca68126b0a7b5c
Cesarq19 <cepaquin@espol.edu.ec>
2022-12-29 23:27:37 -0500

    Organizacion del codigo

--- ProEmbG1/src/main.cpp
+++ ProEmbG1/src/main.cpp
@@ -1,41 +1,65 @@
+/*
+  Proyecto de Sistemas embebidos
+  Medidor de resistencias 1/2W y 1/4W
+*/
+
+//librerias a usar del proyecto
 #include <Arduino.h>
-#include <Wire.h>              //for ESP8266 use bug free i2c driver https://github.com/enjoyneering/ESP8266-I2C-Driver
+#include <Wire.h>      
 #include <LiquidCrystal_I2C.h>
 #include <math.h>
-#include <Tone32.h>
-#include <FS.h>
 #include <WiFi.h>
 #include <FirebaseESP32.h>
 
-
+//variables de la LCD I2C
 #define COLUMS 16
 #define ROWS   2
-
 #define PAGE   ((COLUMS) * (ROWS))
-#define buzzer_c 0
+LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
 
-#define WIFI_SSID "WIFI_AP"
-#define WIFI_PASSWORD "WIFI_PASSWORD"
+// inicializacion de la conexion hacia la base de datos en Firebase
+#define WIFI_SSID "MOVISTAR WIFI5026"
+#define WIFI_PASSWORD "sjiv5595"
+#define API_KEY "AIzaSyBsUDmoqRCqq600MhwRt0SMo1qAdPZ36vo"
+#define FIREBASE_PROJECT_ID "ecuares-7bda5"
+#define FIREBASE_CLIENT_EMAIL "AIzaSyBsUDmoqRCqq600MhwRt0SMo1qAdPZ36vo"
+#define FIREBASE_HOST "https://console.firebase.google.com/project/ecuares-7bda5/database/ecuares-7bda5-default-rtdb/data/~2F"
+#define FIREBASE_AUTH "BNPiLp2vfzcQfmtZIzGGgMVFDTpIT3jL72kl4dGo"
+#define USER_EMAIL "cepaquin@epol.edu.ec"
+#define USER_PASSWORD "1234656to"
 
+// Matriz de factores comerciales de resistencias
 float resCom [14] ={1,1.2,1.5,1.8,2.2,2.4,2.7,3.3,3.9,4.7,5.1,5.6,6.8,8.2};
 
+// Perifericos de salida
 int led_1_4w = 4;
 int led_1_2w = 5;
 int buzzer = 2;
 
-
+// Botones (Perifericos de entrada)
 int bt_modo = 26;
 int bt_aceptar = 27;
 int bt_mostrar = 12;
 
-const int vsensor = 35;
-int valor = 0;
-
-LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
-
+// inicializacion de modos del sistema
 bool modo = true;
 bool aceptar = true;
 bool mostrar = true;
+
+
+FirebaseData firebaseData;
+FirebaseJson json;
+String path = "/esp32";
+
+// Inicializacion del primer ADC (1/4W)
+const int vsensorA = 35;
+int valorA = 0;
+
+// Inicializacion del segundo ADC (1/2W)
+const int vsensorB = 32;
+int valorA = 0;
+
+
 
 void selec_mode(){
   lcd.clear();
@@ -61,7 +85,7 @@ void selec_mode(){
 float escogerRes(float r){
   int i=0;
   for(i=0;i<14;i++){
-    float valor=resCom[i];
+    float valorA=resCom[i];
   
   
       if(r>(resCom[i]-(resCom[i]*0.091) ) && r<(resCom[i]+(resCom[i]*0.09) )){
@@ -88,8 +112,9 @@ float calculoRes(){
   int muestras = 100;
 
   for (int i=0;i<muestras;i++){
-    voltage = voltage + analogRead(vsensor)*(3.313/4095.0);
+    voltage = voltage + analogRead(vsensorA)*(3.313/4095.0);
   }
+  
   voltage = (voltage/muestras)+0.2;
 
   float current = (3.313-voltage)/1000;
@@ -101,19 +126,19 @@ float calculoRes(){
 }
 
 void tono(){
-  tone(buzzer,NOTE_A2,500,buzzer_c);
-  noTone(buzzer,buzzer_c);
-  tone(buzzer,NOTE_D4,500,buzzer_c);
-  noTone(buzzer,buzzer_c);
+  digitalWrite(buzzer,HIGH);
+  delay(50);
+  digitalWrite(buzzer,LOW);
 }
 
 void setup() {
   lcd.begin(COLUMS,ROWS);
-  //Serial.begin(9600);
+  
   pinMode(bt_modo,INPUT_PULLDOWN);
   pinMode(bt_aceptar,INPUT_PULLDOWN);
   pinMode(bt_mostrar,INPUT_PULLDOWN);
-  pinMode(vsensor,INPUT);
+  pinMode(vsensorA,INPUT);
+  pinMode(vsensorB,INPUT);
 
   pinMode(led_1_2w,OUTPUT);
   pinMode(led_1_4w,OUTPUT);
@@ -123,6 +148,23 @@ void setup() {
   lcd.print("**Ecuares**");
   delay(3000);
   lcd.clear();
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  lcd.setCursor(0,0);
+  lcd.print("Connecting Wi-Fi");
+  lcd.setCursor(0,1);
+  while (WiFi.status() != WL_CONNECTED)
+
+  {
+    lcd.print(".");
+
+    delay(300);
+
+  }
+  lcd.clear();
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+  Firebase.setwriteSizeLimit(firebaseData, "tiny");
 }
 
 void loop() {
@@ -159,11 +201,11 @@ void loop() {
         }
       }
       lcd.clear();
-      //valor = analogRead(vsensor);
+      //valorA = analogRead(vsensor);
       float valorRes = calculoRes();
       lcd.setCursor(2,0);
       /*
-        Metodo para pasar el valor a string
+        Metodo para pasar el valorA a string
       */
       lcd.printf("R= %.2f",valorRes);      
       lcd.setCursor(2,1);
